c507f0f72b9712b58f4150668219c2ec6d65bdec
Vitocostrian <74500780+Vitocostrian@users.noreply.github.com>
2021-05-13 14:56:43 +0200

    Create DisplayerApp.java

---
+++ DisplayerApp.java
@@ -0,0 +1,10 @@
+//DisplayerApp.java
+import javax.swing.JOptionPane;
+
+public class DisplayerApp{
+    public static void main(Strings [] args)
+    {
+        JOptionPane.showMessageDialog(null, "Programming is Fun!");
+        System.exit(0);
+    }
+}
